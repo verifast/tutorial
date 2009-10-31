affd4d5823b18f21bac62f1fe649032bbf6f34f6
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2009-10-31 10:59:19 +0000

    - Tutorial solution files now have more meaningful names - Added solution file names to tutorial text

--- tutorial/sum.c
+++ tutorial/sum.c
@@ -31,77 +31,41 @@ struct stack *create_stack()
     return stack;
 }
 
-/*@
-
-predicate lseg(struct node *first, struct node *last, int count) =
-    first == last ?
-        count == 0
-    :
-        0 < count &*& first != 0 &*&
-        first->value |-> _ &*& first->next |-> ?next &*& malloc_block_node(first) &*&
-        lseg(next, last, count - 1);
-
-lemma void nodes_to_lseg_lemma(struct node *first)
-    requires nodes(first, ?count);
-    ensures lseg(first, 0, count);
-{
-    open nodes(first, count);
-    if (first != 0) {
-        nodes_to_lseg_lemma(first->next);
-    }
-    close lseg(first, 0, count);
-}
-
-lemma void lseg_to_nodes_lemma(struct node *first)
-    requires lseg(first, 0, ?count);
-    ensures nodes(first, count);
-{
-    open lseg(first, 0, count);
-    if (first != 0) {
-        lseg_to_nodes_lemma(first->next);
-    }
-    close nodes(first, count);
-}
-
-lemma void lseg_add_lemma(struct node *first)
-    requires lseg(first, ?last, ?count) &*& last != 0 &*& last->value |-> _ &*& last->next |-> ?next &*& malloc_block_node(last) &*& lseg(next, 0, ?count0);
-    ensures lseg(first, next, count + 1) &*& lseg(next, 0, count0);
-{
-    open lseg(first, last, count);
-    if (first == last) {
-        close lseg(next, next, 0);
-    } else {
-        lseg_add_lemma(first->next);
-    }
-    open lseg(next, 0, count0);
-    close lseg(next, 0, count0);
-    close lseg(first, next, count + 1);
-}
-
-@*/
-
-int stack_get_count(struct stack *stack)
+bool stack_is_empty(struct stack *stack)
     //@ requires stack(stack, ?count);
-    //@ ensures stack(stack, count) &*& result == count;
+    //@ ensures stack(stack, count) &*& result == (count == 0);
 {
     //@ open stack(stack, count);
     struct node *head = stack->head;
-    //@ nodes_to_lseg_lemma(head);
-    struct node *n = head;
-    int i = 0;
-    //@ close lseg(head, head, 0);
-    while (n != 0)
-        //@ invariant lseg(head, n, i) &*& lseg(n, 0, count - i);
-    {
-        //@ open lseg(n, 0, count - i);
-        n = n->next;
-        i++;
-        //@ lseg_add_lemma(head);
-    }
-    //@ open lseg(0, 0, _);
-    //@ lseg_to_nodes_lemma(head);
+    //@ open nodes(head, count);
+    bool result = stack->head == 0;
+    //@ close nodes(head, count);
     //@ close stack(stack, count);
-    return i;
+    return result;
+}
+
+int nodes_get_sum(struct node *nodes)
+    //@ requires nodes(nodes, ?count);
+    //@ ensures nodes(nodes, count);
+{
+    int result = 0;
+    //@ open nodes(nodes, count);
+    if (nodes != 0) {
+        result = nodes_get_sum(nodes->next);
+        result += nodes->value;
+    }
+    //@ close nodes(nodes, count);
+    return result;
+}
+
+int stack_get_sum(struct stack *stack)
+    //@ requires stack(stack, ?count);
+    //@ ensures stack(stack, count);
+{
+    //@ open stack(stack, count);
+    int result = nodes_get_sum(stack->head);
+    //@ close stack(stack, count);
+    return result;
 }
 
 void stack_push(struct stack *stack, int value)
@@ -133,11 +97,20 @@ int stack_pop(struct stack *stack)
 }
 
 void stack_dispose(struct stack *stack)
-    //@ requires stack(stack, 0);
+    //@ requires stack(stack, _);
     //@ ensures true;
 {
-    //@ open stack(stack, 0);
-    //@ open nodes(_, _);
+    //@ open stack(stack, _);
+    struct node *n = stack->head;
+    while (n != 0)
+        //@ invariant nodes(n, _);
+    {
+        //@ open nodes(n, _);
+        struct node *next = n->next;
+        free(n);
+        n = next;
+    }
+    //@ open nodes(0, _);
     free(stack);
 }
 
